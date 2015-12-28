e3b1b7c339b05fc5d43749ff4298daa94759d35f
Robi Bhattacharjee <robibhatt@gmail.com>
2015-12-28 14:37:07 -0800

    started beads

---
+++ beads/test.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+
+
+
+# code snippet for illustrating input/output
+
+inp = raw_input()
+N, M = [int(x) for x in inp.split(' ')]
+
+A = []
+B = []
+C = []
+
+for i in range(0, M):
+
+    numbers = raw_input()
+    a, b, c = [int(x) for x in numbers.split(' ')]
+    A.append(a)
+    B.append(b)
+    C.append(c)
+
+print 0
+                   
