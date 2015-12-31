5e236a06043bacb3612e45724e283c2e9d008f7b
Robi Bhattacharjee <robibhatt@gmail.com>
2015-12-30 23:10:13 -0800

    working almost_sorted_intervals

--- beads/test.py
+++ beads/test.py
@@ -1,24 +1,67 @@
 #!/bin/python
+prime = 1000000007
+class Modulo:
+    
+    def __init__(self, p):
+        self.p = p
 
+    def product(self, numbers):
+        sanitized = [x % self.p for x in numbers]
+        answer = 1
+        for s in sanitized:
+            answer = (answer * s) % (self.p)
+        return answer
+
+    def sum(self, numbers):
+        sanitized = [x % self.p for x in numbers]
+        answer = 0
+        for s in sanitized:
+            answer = (answer + s) % (self.p)
+        return answer
+
+    def exponent(self, base, exponent):
+        a = base % self.p
+        b = exponent % (self.p - 1)
+        if b == 0:
+            return 1
+        if a == 0:
+            return 0
+        if a == 1:
+            return 1
+        if b == 1:
+            return (a % self.p)
+        if b == 2:
+            return ((a*a) % self.p)
+        if b % 2 == 0:
+            temp = self.exponent(a, b/2)
+            return ((temp * temp) % self.p)
+        else:
+            temp = self.exponent(a, b/2)
+            return ((temp * temp * a) % self.p)
 
 
 
 # code snippet for illustrating input/output
 
-inp = raw_input()
-N, M = [int(x) for x in inp.split(' ')]
+num_tests = int(raw_input())
+mod = Modulo(prime)
+answers = []
 
-A = []
-B = []
-C = []
 
-for i in range(0, M):
+for i in range(num_tests):
 
-    numbers = raw_input()
-    a, b, c = [int(x) for x in numbers.split(' ')]
-    A.append(a)
-    B.append(b)
-    C.append(c)
+    N = int(raw_input())
+    raw_b = raw_input()
+    b = [int(x) for x in raw_b.split(' ')]
+    stuff = []
+    stuff.append(mod.exponent(mod.sum(b), N-2))
+    for i in range(N):
+        stuff.append(mod.exponent(b[i], b[i]-1))
+    answers.append(mod.product(stuff))
 
-print 0
+for a in answers:
+    print a
+
+
+
                    
